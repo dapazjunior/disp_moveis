a6207a0eeff5cd56eaef1b744379696fac61c2a1
dapazjunior <61951645+dapazjunior@users.noreply.github.com>
2026-06-25 22:24:51 -0300

    atualizado

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // Use a versão 8.11.1 para o plugin com.android.application
+    id("com.android.application") version "8.11.1" apply false
+    // Use a versão 4.3.10 para o plugin do Google Services
+    id("com.google.gms.google-services") version "4.3.10" apply false
+    // Use a versão 2.2.20 para o plugin org.jetbrains.kotlin.android
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    // Remova a declaração de versão para o plugin do Flutter Gradle
+    id("dev.flutter.flutter-gradle-plugin") apply false // Removido 'version "1.0.0"'
+}
+
 allprojects {
     repositories {
         google()
@@ -21,8 +33,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-plugin{
-    id("com.google.gms.google-services") version "4.5.0" apply false
 }
